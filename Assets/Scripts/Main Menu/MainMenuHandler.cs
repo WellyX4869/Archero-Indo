f7a233690aca77e50356eeaec9f0cc3f86b8d50c
Edward Manhattan <c14180100@john.petra.ac.id>
2021-06-21 18:48:54 +0700

    change skill ui

---
+++ Assets/Scripts/Main Menu/MainMenuHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+	public void StartLevel()
+	{
+		SceneManager.LoadScene("Sandbox");
+	}
+	public void Exit()
+	{
+		Debug.Break();
+		Application.Quit();
+	}
+}
