c9162d419558fff2fe6f44a5c22869b5983b3657
WellyX4869 <wellysusanto4869@gmail.com>
2022-11-12 23:11:58 +0700

    Fix to adjust display player on PC to 9:16 aspect ratio

---
+++ Assets/Scripts/UI/MainMenuPanelAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPanelAnimation : MonoBehaviour
+{
+    [Header("Panel")]
+    [SerializeField] Transform mainMenuPanel;
+
+    [Header("Animation")]
+    [SerializeField] Vector3 moveSpeed;
+    [SerializeField] Vector2 boundsX;
+
+    int direction = -1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if moving out of bounds
+        if(mainMenuPanel.localPosition.x <= boundsX.x) { direction = 1; }
+        if(mainMenuPanel.localPosition.x >= boundsX.y) { direction = -1; }
+
+        // Move Panel
+        mainMenuPanel.localPosition += moveSpeed * direction * Time.deltaTime;
+    }
+}
